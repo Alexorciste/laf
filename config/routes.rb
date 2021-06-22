737f1816ea68b3b5e3fc3fca98ce72a72aa643bc
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2021-06-22 12:30:43 +0200

    initapp

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
+  get 'photos/index'
+  get 'photos/show'
+  get 'photos/new'
+  get 'photos/create'
+  get 'photos/edit'
+  get 'photos/update'
+  get 'photos/destroy'
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/create'
+  get 'categories/edit'
+  get 'categories/update'
+  get 'categories/destroy'
   devise_for :users
   root to: 'pages#home'
+
+  resources :categories do
+    resources :photos
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_092651) do
+ActiveRecord::Schema.define(version: 2021_06_22_094914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

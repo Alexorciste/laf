488f526b20f0f0c6f94503bc2f5e23f1ae92881b
Alexorciste <boin.alexandre@wanadoo.fr>
2022-06-24 16:46:13 +0200

    retest

--- app/models/privatecat.rb
+++ app/models/privatecat.rb
@@ -1,6 +1,7 @@
 class Privatecat < ApplicationRecord
     has_many :private_assigns
     has_many_attached :images
+    has_many :owners, class_name: "User", foreign_key: 'gallery_owner_id'
 end
 
 
